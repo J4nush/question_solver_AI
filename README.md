cc60ce52a09898d62b208a4031e5613bbd63dd3c
mziolek <j4nushtr4cz@gmail.com>
2022-12-20 17:01:33 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,25 @@
+# question_solver_AI
+
+question_solver is a simple script written in python that, by using the API from OpenAI,
+generates answers to questions (contained in the questions.txt file) and saves them in the result.txt file.
+
+## usage
+The first thing you need to do to use the script is to download your own API key from the OpenAI portal, and put it in line 6 of main.py
+```python
+token = "<YOUR-API-TOKEN-HERE>"
+```
+
+The second step is to create a questions.txt file, which will contain the questions.
+
+!!!NOTE!!!
+ONE LINE IN THE FILE IS ONE QUESTION!!!
+(Sample file included in the repository)
+
+## Contributing
+
+You can rework the script as you wish, the way of reading and writing is up to you (I wanted to work out quickly). The only thing I ask is not to sell this "masterpiece", just make this repository available. Pull requests are possible, but I do not know if I will remember it.
+
+
+
+GLHF
+
--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import requests
 
 
 def generate(text):
+    print(text)
     token = "<YOUR-API-TOKEN-HERE>"
+    model = "text-davinci-003"
+    temperature = 0.8
+    max_tokens = 4000
 
-    data_send = '{"model":"text-davinci-003","prompt": "'+text.rstrip()+'" ,"temperature":0.8, "max_tokens": 4000, "n":1 , "stream": false}'
+    data_send = '{"model": "'+model+'","prompt": "'+text.rstrip()+'" ,"temperature":'+temperature+', "max_tokens": '+max_tokens+', "n":1 , "stream": false}'
     data_send = json.loads(data_send)
     headers = {'Content-Type' : 'application/json; charset=utf-8', 'Authorization': 'Bearer '+token}
     try:
         response = requests.post('https://api.openai.com/v1/completions', json=data_send, headers=headers)
         data_get = response.json()
-        print(response.text)
-        # print(data_get["choices"][0]["text"])
         return data_get["choices"][0]["text"]
     except:
         return "error"
-
+    print("DONE")
 
 def writeLine(question, answer):
     f = open('result.txt', 'a', encoding="utf-8")
@@ -29,21 +31,16 @@ def writeLine(question, answer):
 
 
 def openFile(filename):
-    arr = {}
     f = open(filename, 'r', encoding="utf-8")
     lines = f.readlines()
     for line in lines:
         # print(type(line))
         answer = generate(line)
-        if(answer != "error"):
-            writeLine(line, answer)
-        else:
-            writeLine(line, "ERROR")
-        # arr[line] = answer
-
-    return arr
+        writeLine(line, answer)
+    print("")
 
 if __name__ == '__main__':
-    result = openFile("questions.txt")
+    print("")
+    result = openFile("questions-example.txt")
 
 #by J4nush
